b5134572fe03aaf0a3d715249b852ddeec35c730
karaman56 <166434324+karaman56@users.noreply.github.com>
2025-03-08 00:39:06 +0500

    setup debug toolbar.
     setup debug toolbar.

--- sensive_blog/urls.py
+++ sensive_blog/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from blog import views
-from django.urls import path
-
-from django.conf.urls.static import static
+from django.urls import path, include  # Добавьте include
 from django.conf import settings
+from django.conf.urls.static import static
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,4 +12,12 @@ urlpatterns = [
     path('contacts/', views.contacts, name='contacts'),
     path('', views.index, name='index'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns = [
+        path('__debug__/', include('debug_toolbar.urls')),
+    ] + urlpatterns
+
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
